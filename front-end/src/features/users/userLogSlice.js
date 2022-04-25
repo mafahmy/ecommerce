c3445e601102bbf3cc41422c36aad9c41faf102f
Mahmoud Fahmy <mafahmypc@hotmail.com>
2022-04-25 06:11:33 +0200

    logslice navbar

--- front-end/src/features/users/userLogSlice.js
+++ front-end/src/features/users/userLogSlice.js
@@ -2,39 +2,41 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import axios from "axios";
 
+const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
 export const login = createAsyncThunk(
   "USER_LOGIN",
   async ({ email, password }, thunkAPI) => {
     try {
-      const  { data }  = await axios.post(
+      const { data } = await axios.post(
         "http://localhost:4000/api/users/signin",
         { email, password }
       );
-    
+
       localStorage.setItem("userInfo", JSON.stringify(data));
       return data;
     } catch (error) {
       const message =
         error.response && error.response.data.message
-        ? error.response.data.message
-        : error.message;
-       
-       return thunkAPI.rejectWithValue(message);
+          ? error.response.data.message
+          : error.message;
 
+      return thunkAPI.rejectWithValue(message);
     }
   }
 );
 
 export const logout = createAsyncThunk("USER_LOGOUT", () => {
-  localStorage.removeItem('userInfo');
-  localStorage.removeItem('cartItems');
-})
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
+});
 
-const initialState = 
- {
-   isLoggedIn: false
- } 
+const initialState = userInfo
+  ? {
+      isLoggedIn: true,
+      userInfo,
+    }
+  : { isLoggedIn: false, userInfo: null };
 
 const userLogSlice = createSlice({
   name: "log",
@@ -45,39 +47,32 @@ const userLogSlice = createSlice({
       // state.isLoading = true;
       return {
         isLoading: true,
-        isLoggedIn: false
-      }
+        isLoggedIn: false,
+      };
     },
     [login.fulfilled]: (state, action) => {
       return {
         isLoading: false,
         isLoggedIn: true,
         userInfo: action.payload,
-        
-      }
-
-      
+      };
     },
     [login.rejected]: (state, action) => {
       return {
         isLoading: false,
         isLoggedIn: false,
-        
-        error: action.payload,
-      }
-      
-    },
-  
-    [logout.fulfilled]: (state, action) => {
-      
-     return {
-       isLoading: false,
-      // userInfo: {},
-       isLoggedIn: false
 
-     }
+        error: action.payload,
+      };
     },
-  
+
+    [logout.fulfilled]: (state, action) => {
+      return {
+        isLoading: false,
+         userInfo: null,
+        isLoggedIn: false,
+      };
+    },
   },
 });
 export default userLogSlice.reducer;
