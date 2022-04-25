b12576715f6225d214741c7b98159c2a7ca554f8
Mahmoud Fahmy <mafahmypc@hotmail.com>
2022-04-25 07:53:47 +0200

    removing unnecessary

--- front-end/src/features/users/userDetailsSlice.js
+++ front-end/src/features/users/userDetailsSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { setMessage } from "../messages/messageSlice";
+
 import axios from "axios";
 
 export const detailsUser = createAsyncThunk(
@@ -19,13 +19,11 @@ export const detailsUser = createAsyncThunk(
       return data;
     } catch (error) {
       const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
-      thunkAPI.dispatch(setMessage(message));
-      return message;
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+
+      return thunkAPI.rejectWithValue(message);
     }
   }
 );
