bfcb3a3c7d53039b1a59318e476143e28d540c52
Mahmoud Fahmy <mafahmypc@hotmail.com>
2022-04-26 18:04:49 +0200

    - working on LayOut , Admin pay testing Admin deliver testing, adding scroll to to componenet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,7 @@ import ShippingAddressScreen from "./screens/ShippingAdressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import UsersScreen from "./screens/admin/users/UsersScreen";
 //import SideBar from "./components/admin/sideBar/SideBar";
@@ -23,93 +23,118 @@ import ProductEditScreen from "./screens/admin/products/productEditScreen";
 import OrdersListScreen from "./screens/admin/orders/OrdersListScreen";
 import UserEditScreen from "./screens/admin/users/UserEditScreen";
 import SearchScreen from "./screens/SearchScreen";
-
-
+import ScrollToTop from "./components/ScrollToTop";
 import Navbar1 from "./components/Navbar1";
 
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#203040",
+    },
+    secondary: {
+      main: "#f0c040",
+    },
+  },
+});
 
 function App() {
-  
+ 
   return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="App-header">
-          <Navbar1 />
-        </header>
-        
-        <main>
-          <Routes>
-            <Route path="/cart/:id" element={<CartScreen />} />
-            {/* <Route path="/cart/" element={<CartScreen />} /> */}
-          
-            
-            <Route path="/" element={<HomePage />} exact />
-            <Route path="/product/:id" exact element={<ProductScreen />} />
-            <Route path="/signin" element={<SigninScreen />} />
-            <Route path="/register" element={<RegisterScreen />} />
-            <Route path="/shipping" element={<ShippingAddressScreen />} />
-            <Route path="/payment" element={<PaymentMethodScreen />} />
-            <Route path="/placeorder" element={<PlaceOrderScreen />} />
-            <Route path="/order/:id" element={<OrderScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-            <Route path="/ordershistory" element={<OrderHistoryScreen />} />
-            <Route path="/search/name/:name?" element={<SearchScreen />} exact />
-            <Route path="/search/category/:category" element={<SearchScreen />} exact />
-            <Route path="/search/brand/:brand" element={<SearchScreen />} exact />
-            <Route
-              path="/productlist"
-              element={
-                <AdminRoute>
-                  <ProductsListScreen />{" "}
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/users"
-              element={
-                <AdminRoute>
-                  {" "}
-                  <UsersScreen />{" "}
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/user/:id/edit"
-              element={
-                <AdminRoute>
-                  {" "}
-                  <UserEditScreen />{" "}
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/orderlist"
-              element={
-                <AdminRoute>
-                  {" "}
-                  <OrdersListScreen />{" "}
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/product/:id/edit"
-              element={
-                <AdminRoute>
-                  {" "}
-                  <ProductEditScreen />{" "}
-                </AdminRoute>
-              }
-              exact
-            />
+    <ThemeProvider theme={theme}>
+      
+      <BrowserRouter>
+      <ScrollToTop />
+        <div className="grid-container">
+          <header className="App-header">
+            <Navbar1 />
+          </header>
 
-            
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </BrowserRouter>
+          <main>
+            <Routes>
+              <Route path="/cart/:id" element={<CartScreen />} />
+              {/* <Route path="/cart/" element={<CartScreen />} /> */}
+
+              <Route path="/" element={<HomePage />} exact />
+              <Route path="/product/:id" exact element={<ProductScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/register" element={<RegisterScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/ordershistory" element={<OrderHistoryScreen />} />
+              <Route
+                path="/search/name/:name?"
+                element={<SearchScreen />}
+                exact
+              />
+              <Route
+                path="/search/category/:category"
+                element={<SearchScreen />}
+                exact
+              />
+              <Route
+                path="/search/brand/:brand"
+                element={<SearchScreen />}
+                exact
+              />
+              <Route
+                path="/productlist"
+                element={
+                  <AdminRoute>
+                    <ProductsListScreen />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/users"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <UsersScreen />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/user/:id/edit"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <UserEditScreen />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/orderlist"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <OrdersListScreen />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/product/:id/edit"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <ProductEditScreen />{" "}
+                  </AdminRoute>
+                }
+                exact
+              />
+            </Routes>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+        {/* </ScrollToTop> */}
+      </BrowserRouter>
+
+    </ThemeProvider>
   );
 }
 
--- front-end/src/screens/HomePage.js
+++ front-end/src/screens/HomePage.js
@@ -1,53 +1,30 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import styled from "styled-components";
-// import Product from "../components/Product";
+import React from "react";
+
 import Categories from "../components/Categories";
 import Slider1 from "../components/Slider1";
-// import { useGetAllProductsQuery } from "../features/products/productsApi";
+
 import Brands from "../components/Brands";
 
-// const Container = styled.div `
-//   height: max-content;
-//   display: flex;
-//   flex-wrap: wrap;
-//   align-items: center;
-//   justify-content: center;
-// `;
+
+
+
 
 const HomePage = () => {
-   //const [products, setProducts] = useState([]);
+   
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const { data } = await axios.get("http://localhost:4000/api/products");
-  //       setProducts(data);
-  //       console.log(data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   fetchData();
-  //   console.log(products);
-  // }, []);
-  // const { data, error, isLoading, isFetching, isSuccess } =  useGetAllProductsQuery([]);
- 
-  // console.log(data);
+
 
   return (
+    <>
+    
     <div>
     <Slider1 />
     <Categories />
     <Brands />
-      {/* <Container>
-        {isLoading ? (<p>Loading...</p>): error ? (<p>error{error}</p>) : (
-        data?.map((product) => (
-          <Product key={product._id} product={product} />
-        ))
-        ) }
-      </Container> */}
     </div>
+    
+    
+    </>
   );
 };
 export default HomePage;
